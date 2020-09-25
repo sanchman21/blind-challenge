c4ebdee3f41437838b3ea26c4cd9d3212b00df03
sanchman21 <goelsanchit21@gmail.com>
2020-09-26 00:50:12 +0530

    updated setting.py

--- techniosys/urls.py
+++ techniosys/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.editor, name='editor'),
-]
+    path('', views.editor, name='editor')
+] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
